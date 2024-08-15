4a7959eecc57b547131af3acee02c628168db9ae
Aurghyadip Kundu <aurghya.blog@gmail.com>
2024-08-15 18:36:11 +0530

    added new changeS

--- unmerge_sheets.py
+++ unmerge_sheets.py
@@ -11,7 +11,12 @@ for file in os.listdir(directory):
     if filename.endswith(".xlsx"):
         excel_files.append(filename)
 
-print(excel_files)
+for file in excel_files:
+    df = pd.read_excel("excel/" + file)
+    df = df.fillna(method='ffill')
+    df.to_excel("excel_modified/" + file, index=False)
+
+
 
 # df_cse = pd.read_csv("CSE.csv")
 
