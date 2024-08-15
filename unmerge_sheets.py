c47f0e3a989e879aaa238e8cb5c1a7d129bf5695
Aurghyadip Kundu <aurghya.blog@gmail.com>
2024-08-15 18:37:01 +0530

    Added new changes

--- unmerge_sheets.py
+++ unmerge_sheets.py
@@ -15,11 +15,3 @@ for file in excel_files:
     df = pd.read_excel("excel/" + file)
     df = df.fillna(method='ffill')
     df.to_excel("excel_modified/" + file, index=False)
-
-
-
-# df_cse = pd.read_csv("CSE.csv")
-
-# df_cse = df_cse.fillna(method='ffill')
-
-# df_cse.to_excel("CSE_Modified.xlsx", index=False)
