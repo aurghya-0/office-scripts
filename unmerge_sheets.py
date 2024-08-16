ed366080ad94dbb0f86e2dd47b029bb5662f6a92
Aurghyadip Kundu <aurghya.blog@gmail.com>
2024-08-16 07:53:01 +0530

    added function

--- unmerge_sheets.py
+++ unmerge_sheets.py
@@ -1,17 +1,20 @@
 import os
 import pandas as pd
 
-directory = os.fsencode("excel")
+def unmerge_cells(directory):
+    excel_files = []
 
-excel_files = []
+    for file in os.listdir(directory):
+        filename = os.fsdecode(file)
+        if filename.endswith(".xlsx"):
+            excel_files.append(filename)
 
-for file in os.listdir(directory):
-    filename = os.fsdecode(file)
-    print(filename)
-    if filename.endswith(".xlsx"):
-        excel_files.append(filename)
+    for file in excel_files:
+        df = pd.read_excel(directory + file)
+        df = df.fillna(method='ffill')
+        df.to_excel(directory + "modified/" + file, index=False)
 
-for file in excel_files:
-    df = pd.read_excel("excel/" + file)
-    df = df.fillna(method='ffill')
-    df.to_excel("excel_modified/" + file, index=False)
+
+if __name__ == "__main__":
+    directory = "excel"
+    unmerge_cells(directory)
